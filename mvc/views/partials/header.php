38c531f603b6ca05a6f34e8dff2962ea340e42dc
minhtuan5422 <tuan.nm@lptech.asia>
2023-09-21 15:17:24 +0700

    Update url fonts icon

--- mvc/views/partials/header.php
+++ mvc/views/partials/header.php
@@ -1,32 +1,31 @@
 <header class="header">
-    <div class="header-expands">
-        <a href="#">
+    <div class="header__top">
+        <div class="header__top--flex container-xl d-flex justify-content-between align-content-center">
+          <a href="#" class="header__top--flex--maps">
             <i class="icon-location"></i>
             <span>Store Location: Lincoln- 344, Illinois, Chicago, USA</span>
-        </a>
-
-        <div class="header-expands__action">
-          <div class="dropdown">
-            <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-              Dropdown button
-            </button>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Normal</a></li>
-              <li><a class="dropdown-item active" href="#">Active</a></li>
-              <li><a class="dropdown-item disabled" href="#">Disabled</a></li>
-            </ul>
-          </div>
+          </a>
+  
+          <div class="header-expands__action">
+            <select class="border-0" name="" id="">
+              <option value="">Eng</option>
+              <option value="">Vie</option>
+            </select>
+  
+            <select class="border-0" name="" id="">
+              <option value="">USD</option>
+              <option value="">VND</option>
+            </select>
         </div>
     </div>
 
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
       <div class="container-xl">
         <a class="navbar-brand" href="javascript:void(0)">Logo</a>
-        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
-    
+      
         <div class="collapse navbar-collapse" id="mynavbar">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
