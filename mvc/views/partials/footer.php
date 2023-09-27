f010312894ef823c084e2656943f8f73c7a21e5f
minhtuan5422 <tuan.nm@lptech.asia>
2023-09-27 22:32:56 +0700

    Update footer and file font icon

--- mvc/views/partials/footer.php
+++ mvc/views/partials/footer.php
@@ -1,23 +1,70 @@
 <div class="footer">
     <div class="container-xl">
         <div class="row">
-            <div class="col-sm-4">
-                <div class="">
-                    <img src=<?php echo IMG_PATH . "Logo.png" ?> alt="" srcset="">
+            <div class="footer__company col-sm-4">
+                <div class="footer__company--logo">
+                    <img src=<?php echo IMG_PATH . "Logo-footer.png" ?> alt="" srcset="">
                 </div>
 
-                <p>Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis dui, eget bibendum magna congue nec.</p>
+                <p class="footer__company--desc">Morbi cursus porttitor enim lobortis molestie. Duis gravida turpis dui, eget bibendum magna congue nec.</p>
 
-                <div class="">
-                    <span>(219) 555-0114</span>
-                    <p>or</p>
-                    <span>Proxy@gmail.com</span>
+                <div class="footer__company--action d-flex align-items-center gap-2">
+                    <a href="#"><i class="icon-facebook"></i></a>
+                    <a href="#"><i class="icon-youtube"></i></a>
+                    <a href="#"><i class="icon-instagram"></i></a>
+                    <a href="#"><i class="icon-map"></i></a>
                 </div>
             </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-2"></div>
+
+            <div class="footer__link col-sm-2">
+                <p class="footer__link--title">My Account</p>
+                <div class="footer__link--divider"></div>
+                <ul class="footer__link--list">
+                    <li class="footer__link--list--item">My Account</li>
+                    <li class="footer__link--list--item">Order History</li>
+                    <li class="footer__link--list--item">Shoping Cart</li>
+                    <li class="footer__link--list--item">Wishlist</li>
+                </ul>
+            </div>
+
+            <div class="footer__link col-sm-2">
+                <p class="footer__link--title">Helps</p>
+                <div class="footer__link--divider"></div>
+                <ul class="footer__link--list">
+                    <li class="footer__link--list--item">Contact</li>
+                    <li class="footer__link--list--item">Faqs</li>
+                    <li class="footer__link--list--item">Terms & Condition</li>
+                    <li class="footer__link--list--item">Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div class="footer__link col-sm-2">
+                <p class="footer__link--title">Proxy</p>
+                <div class="footer__link--divider"></div>
+                <ul class="footer__link--list">
+                    <li class="footer__link--list--item">About</li>
+                    <li class="footer__link--list--item">Shop</li>
+                    <li class="footer__link--list--item">Product</li>
+                    <li class="footer__link--list--item">Track Order</li>
+                </ul>
+            </div>
+
+            <div class="footer__link col-sm-2">
+                <p class="footer__link--title">Categories</p>
+                <div class="footer__link--divider"></div>
+                <ul class="footer__link--list">
+                    <li class="footer__link--list--item">Fruit & Vegetables</li>
+                    <li class="footer__link--list--item">Meat & Fish</li>
+                    <li class="footer__link--list--item">Bread & Bakery</li>
+                    <li class="footer__link--list--item">Beauty & Health</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="footer__copyright">
+            <p>Ecobazar eCommerce © 2021. All Rights Reserved</p>
+            <img src=<?php echo IMG_PATH . "payment.png";?> alt="" srcset="">
         </div>
     </div>
 </div>
