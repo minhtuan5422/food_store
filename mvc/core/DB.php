840c76eeac9213b74cd0590d8b2f0ab8ddb7a492
minhtuan5422 <tuan.nm@lptech.asia>
2023-11-11 10:21:27 +0700

    Update class database, function select insert update delete data

--- mvc/core/DB.php
+++ mvc/core/DB.php
@@ -1,15 +1,86 @@
 <?php
     class DB {
-  /*       public $conn;
+        public $conn;
         protected $servername = 'localhost';
         protected $username = 'root';
         protected $password = '';
-        protected $dbName = "mvc";
+        protected $dbName = "food_store";
 
         function __construct() {
             $this->conn = mysqli_connect($this->servername, $this->username, $this->password);
             mysqli_select_db($this->conn, $this->dbName);
             mysqli_query($this->conn, "SET NAMES 'utf8'");
-        } */
+
+            if($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        }
+
+        public function query($sql) {
+            return $this->conn->query($sql);
+        }
+
+        public function select($sql) {
+            $result = $this->query($sql);
+            $data = array();
+
+            while($row = mysqli_fetch_array($result)) {
+                $data[] = $row;
+            }
+
+            return $data;
+        }
+
+        public function insert($table, $data) {
+            $keys = array_keys($data);
+            $values = array_values($data);
+            $keys = array_map(array($this, "escape"), $keys);
+            $values = array_map(array($this, "escape"), $values);
+            $keys = implode(",", $keys);
+            $values = "'" . implode("','", $values) . "'";
+            $sql = "INSERT INTO " . $table . " (" . $keys . ") VALUES (" . $values . ")";
+            $this->query($sql);
+        }
+
+        public function delete($table, $where) {
+            $sql = "DELETE FROM $table WHERE $where";
+            $result = $this->query($sql);
+            return $result;
+        }
+
+        public function update($table, $data, $where) {
+            $set = [];
+            foreach ($data as $column => $value) {
+              $set[] = "$column = '$value'";
+            }
+            $set = implode(', ', $set);
+            $sql = "UPDATE $table SET $set WHERE $where";
+            return $this->query($sql);   
+        }
+
+        public function search($table, $field, $keyword, $joinTables = [], $joinConditions = []) {
+            $sql = "SELECT * FROM $table";
+            $joinSql = "";
+          
+            $numJoins = count($joinTables);
+            if ($numJoins > 0) {
+              for ($i = 0; $i < $numJoins; $i++) {
+                $joinTable = $joinTables[$i];
+                $joinCondition = $joinConditions[$i];
+                $joinSql .= " JOIN $joinTable ON $joinCondition";
+              }
+              $sql .= $joinSql;
+            }
+          
+            $sql .= " WHERE $table.$field LIKE ?";
+          
+            $stmt = $this->conn->prepare($sql);
+            $keyword = "%$keyword%";
+            $stmt->bind_param("s", $keyword);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+          
+            return $result;
+          }
     }
-?>
